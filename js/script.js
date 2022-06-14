fce13e61d63f6d190d3243068ecb966ea5e78f94
zhLisha <lisazhou1709@gmail.com>
2022-06-14 14:08:54 +0200

    promt: ask user the lv wanted, set max number for each levels

--- js/script.js
+++ js/script.js
@@ -7,3 +7,30 @@
 */
 
 
+// Scenta dell'utente per la difficolta' del gioco 
+// Se sceglie lv.1 i numeri sono compresi tra 1-100
+// Se sceglie lv.2 i numeri sono compresi tra 1-81
+// Se sceglie lv.3 i numeri sono compresi tra 1-49
+const userLv = prompt('Scegli la difficoltà tra 1 - 2 - 3')
+console.log('Livello scento da utente:', userLv);
+
+// Creo una variabile con i numeri massimi per ogni livello da inserire poi nella generazione di numeri random per livello
+let maxNumber;
+if(userLv === "1") {
+    maxNumber = 100;
+} else if(userLv === "2") {
+    maxNumber = 81;
+} else if(userLv === "3") {
+    maxNumber = 49;
+}
+console.log('Livello massimo', maxNumber);
+
+
+// Genero 16 numeri random tra quelli del livello scento dall'utente 
+// Inserisco i 16 numeri NON DUPLICATI nella nostra lista delle bombe
+
+// Chiedere all'utente un numero tra quelli del livello scelto
+// Se inserisce un numero compreso nella lista delle bombe allora: fine del gioco
+// Altrimenti continua il gioco finche' non finisce tutti i tentativi: ha vinto
+
+// Comunicare all'utente se ha vinto o perso + punteggio dei tentativi giusti che ha fatto
