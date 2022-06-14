25cca7657fe0aa0dc73e37092f21cc3fc3ee5a7f
zhLisha <lisazhou1709@gmail.com>
2022-06-14 14:06:06 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/*
+- L'utente fa una scelta tra 3 difficolta': lv.1 (1-100), lv.2 (1-81), lv.3 (1-49)
+- Generare 16 numeri random da impostare come bombe senza duplicati
+- Attraverso i promt chiedere all'utente di inserire dei numeri tra quelli della difficolta' scelta finche' non finisce il gioco: inserire nel tracciamento dei punti solo i numeri NON DUPLICATI
+- Il gioco e' finito se sceglie un numero bomba (ha perso) o se esaurisce tutte le sue possibilita' (ha vinto)
+- Comunicare al termine della partita il punteggio dell'utente ( cioe' il numero di tentativi giusti prima della bomba) + un messaggio di vittoria e sconfitta in base all'esito.
+*/
+
+
